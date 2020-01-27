015fb8a4a49eef838df26ca52c171af79e36d403
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2020-01-27 14:05:50 +0100

    asd

--- src/landingpage/story.js
+++ src/landingpage/story.js
@@ -15,7 +15,7 @@ story.add('to Pattern Library', () => {
   const wrapper = document.createElement('div');
 
   const template = html`
-    <axa-heading rank="3">Welcome to the Pattern Library!</axa-heading>
+    <axa-heading rank="3">Hallo!</axa-heading>
     <axa-text variant="size-3">
       You can find all our Webcomponents here. Check out our Readme below.
     </axa-text>
