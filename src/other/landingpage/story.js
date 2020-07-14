f9ed9022ce0622617e42fa2eca49fdbecc68ed94
Noah Liechti <noah.liechti@gmail.com>
2020-07-14 10:02:18 +0200

    add base structure

--- src/other/landingpage/story.js
+++ src/other/landingpage/story.js
@@ -18,10 +18,17 @@ story.add('to Pattern Library', () => {
   const wrapper = document.createElement('div');
 
   const template = html`
-    <axa-heading rank="3">Welcome to the Pattern Library!</axa-heading>
-    <axa-text variant="size-3">
-      You can find all our Webcomponents here. Check out our Readme below.
-    </axa-text>
+    <axa-container>
+      <axa-heading rank="3">Welcome to the Pattern Library!</axa-heading>
+      <axa-text variant="size-3">
+        Welcome to the Design System of AXA Switzerland. Our Pattern Library
+        provides components and tools to help product teams work more
+        efficiently, and to make AXA’s applications more cohesive.
+      </axa-text>
+      <axa-heading rank="4">Last releases</axa-heading>
+      <axa-heading rank="4">How to contribute</axa-heading>
+      <axa-heading rank="4">Contact</axa-heading>
+    </axa-container>
   `;
 
   render(template, wrapper);
